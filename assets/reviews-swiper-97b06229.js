f47d6e1c5d9e618927b8ae06bed05f60065db639
bonny-art <svitlana.otenko@gmail.com>
2024-08-07 07:03:49 +0300

    Change gallery

---
+++ assets/reviews-swiper-97b06229.js
@@ -0,0 +1,2 @@
+import{S as e}from"./vendor-4b5ab9a6.js";new e(".reviews-swiper-container",{debugger:!0,breakpoints:{320:{slidesPerView:1,spaceBetween:24},1440:{slidesPerView:4,spaceBetween:40}},loop:!0});
+//# sourceMappingURL=reviews-swiper-97b06229.js.map
