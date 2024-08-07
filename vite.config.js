379370ad2b933b11b2e81526a2119d3997e4453f
bonny-art <svitlana.otenko@gmail.com>
2024-08-07 03:22:31 +0300

    change dist

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig(({ command }) => {
           entryFileNames: 'commonHelpers.js',
         },
       },
-      outDir: 'dist',
+      outDir: '../dist',
     },
     plugins: [
       injectHTML(),
